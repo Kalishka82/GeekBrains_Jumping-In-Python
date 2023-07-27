0fa75ad27413ed3fb768cc142da1ada01e624eae
Tatiana Kalinina <tankakalinka@gmail.com>
2023-07-27 18:32:52 +0400

    homework_06 added after seminar_06

---
+++ ht_06_moduls/ex_01.py
@@ -0,0 +1,33 @@
+# Задание No1
+# 📌 В модуль с проверкой даты добавить возможность запуска в терминале с
+# передачей даты на проверку.
+
+from sys import argv
+
+__all__ = ['check_date']
+
+
+def if_leap(year: int) -> bool:
+    return not(year % 4 != 0 or year % 100 == 0 and year % 400 != 0)
+
+
+def check_date(str_date: str) -> bool:
+    day, mon, year = map(int, str_date.split('.'))
+    if not (1 <= day <= 31 and 1 <= mon <= 12 and 1 <= year <= 9999):
+        return False
+
+    if mon in (4, 6, 9, 11) and day > 30:
+        return False
+
+    if mon == 2 and if_leap(year) and day > 29:
+        return False
+
+    if mon == 2 and not if_leap(year) and day > 28:
+        return False
+
+    return True
+
+
+if __name__ == '__main__':
+    name, *param = argv
+    print(check_date(*(str(elem) for elem in param)))
---
+++ ht_06_moduls/ex_03.py
@@ -0,0 +1,45 @@
+# Задание No3
+# 📌 Написать функцию в шахматный модуль.
+# 📌 Использовать генератор случайных чисел для случайной расстановки ферзей в
+# задаче выше.
+# 📌 Проверить различные случайные варианты и вывести 4 успешных расстановки.
+
+from random import randint
+
+__all__ = ['set_queens', 'isCapture', 'show_success_cases']
+
+N = 8
+NUM_CASES = 4
+
+x, y = [0] * N, [0] * N
+
+
+def set_queens(n=N):
+    for i in range(n):
+        x[i], y[i] = randint(1, N + 1), randint(1, N + 1)
+    return x, y
+
+
+def isCapture(n=N):
+    set_queens(n)
+    for i in range(n):
+        for j in range(i + 1, n):
+            if x[i] == x[j] or y[i] == y[j] or \
+                    abs(x[i] - x[j]) == abs(y[i] - y[j]):
+                return False
+            else:
+                return True
+
+
+def show_success_cases(cases=NUM_CASES):
+    success_cases = []
+    while cases > 0:
+        if isCapture():
+            success_cases.append([*zip(x, y)])
+            cases -= 1
+    return success_cases
+
+
+if __name__ == '__main__':
+    for i, coord_list in enumerate(show_success_cases(), start=1):
+        print(f'{i}: {coord_list}')
